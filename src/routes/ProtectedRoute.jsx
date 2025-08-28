f3acd720fb78149e181e32376226969f72c674e1
MateooMoran <brandon.moran@epn.edu.ec>
2025-08-28 11:08:36 -0500

    Despliegue

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import {Navigate,Outlet} from 'react-router-dom'
 import storeAuth from '../context/storeAuth';
 
+
 const ProtectedRoute = () =>{
     const token = storeAuth((state) => state.token);
     return token ? <Outlet/> : <Navigate to="/" replace />
